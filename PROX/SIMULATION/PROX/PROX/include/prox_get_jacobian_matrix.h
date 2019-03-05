697263403eb3e8f48ba833c59771d06898b487b3
Kenny Erleben <kenny.erleben@gmail.com>
2019-03-05 08:52:43 +0100

    Playing with parameters

--- PROX/SIMULATION/PROX/PROX/include/prox_get_jacobian_matrix.h
+++ PROX/SIMULATION/PROX/PROX/include/prox_get_jacobian_matrix.h
@@ -37,10 +37,10 @@ namespace prox
     
     for(contact_iterator contact = begin; contact != end; ++contact, ++k)
     {
-      vector3_type const n_k = contact->get_normal();
-      vector3_type t_k;
       vector3_type s_k;
-      
+      vector3_type t_k;
+      vector3_type const n_k = contact->get_normal();
+
       body_type const * const body_i = contact->get_body_i();
       body_type const * const body_j = contact->get_body_j();
 
@@ -71,9 +71,9 @@ namespace prox
 
       // Fill in J_ki
       vector3_type r_ki = contact->get_position() - body_i->get_position();
-      vector3_type iXn = tiny::cross( r_ki, n_k );
-      vector3_type iXt = tiny::cross( r_ki, t_k );
       vector3_type iXs = tiny::cross( r_ki, s_k );
+      vector3_type iXt = tiny::cross( r_ki, t_k );
+      vector3_type iXn = tiny::cross( r_ki, n_k );
 
       J_ki(0,0) = -n_k(0);
       J_ki(0,1) = -n_k(1);
@@ -81,18 +81,21 @@ namespace prox
       J_ki(0,3) = -iXn(0);
       J_ki(0,4) = -iXn(1);
       J_ki(0,5) = -iXn(2);
-      J_ki(1,0) = -t_k(0);
-      J_ki(1,1) = -t_k(1);
-      J_ki(1,2) = -t_k(2);
-      J_ki(1,3) = -iXt(0);
-      J_ki(1,4) = -iXt(1);
-      J_ki(1,5) = -iXt(2);
-      J_ki(2,0) = -s_k(0);
-      J_ki(2,1) = -s_k(1);
-      J_ki(2,2) = -s_k(2);
-      J_ki(2,3) = -iXs(0);
-      J_ki(2,4) = -iXs(1);
-      J_ki(2,5) = -iXs(2);
+      
+      J_ki(1,0) = -s_k(0);
+      J_ki(1,1) = -s_k(1);
+      J_ki(1,2) = -s_k(2);
+      J_ki(1,3) = -iXs(0);
+      J_ki(1,4) = -iXs(1);
+      J_ki(1,5) = -iXs(2);
+
+      J_ki(2,0) = -t_k(0);
+      J_ki(2,1) = -t_k(1);
+      J_ki(2,2) = -t_k(2);
+      J_ki(2,3) = -iXt(0);
+      J_ki(2,4) = -iXt(1);
+      J_ki(2,5) = -iXt(2);
+      
       J_ki(3,0) = 0.0f;
       J_ki(3,1) = 0.0f;
       J_ki(3,2) = 0.0f;
@@ -104,28 +107,31 @@ namespace prox
       
       // Fill in J_kj
       vector3_type r_kj = contact->get_position() - body_j->get_position();
-      vector3_type jXn = tiny::cross( r_kj, n_k );
-      vector3_type jXt = tiny::cross( r_kj, t_k );
       vector3_type jXs = tiny::cross( r_kj, s_k );
-      
+      vector3_type jXt = tiny::cross( r_kj, t_k );
+      vector3_type jXn = tiny::cross( r_kj, n_k );
+
       J_kj(0,0) = n_k(0);
       J_kj(0,1) = n_k(1);
       J_kj(0,2) = n_k(2);
       J_kj(0,3) = jXn(0);
       J_kj(0,4) = jXn(1);
       J_kj(0,5) = jXn(2);
-      J_kj(1,0) = t_k(0);
-      J_kj(1,1) = t_k(1);
-      J_kj(1,2) = t_k(2);
-      J_kj(1,3) = jXt(0);
-      J_kj(1,4) = jXt(1);
-      J_kj(1,5) = jXt(2);
-      J_kj(2,0) = s_k(0);
-      J_kj(2,1) = s_k(1);
-      J_kj(2,2) = s_k(2);
-      J_kj(2,3) = jXs(0);
-      J_kj(2,4) = jXs(1);
-      J_kj(2,5) = jXs(2);
+      
+      J_kj(1,0) = s_k(0);
+      J_kj(1,1) = s_k(1);
+      J_kj(1,2) = s_k(2);
+      J_kj(1,3) = jXs(0);
+      J_kj(1,4) = jXs(1);
+      J_kj(1,5) = jXs(2);
+
+      J_kj(2,0) = t_k(0);
+      J_kj(2,1) = t_k(1);
+      J_kj(2,2) = t_k(2);
+      J_kj(2,3) = jXt(0);
+      J_kj(2,4) = jXt(1);
+      J_kj(2,5) = jXt(2);
+      
       J_kj(3,0) = 0.0f;
       J_kj(3,1) = 0.0f;
       J_kj(3,2) = 0.0f;
